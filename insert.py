84f865f54ee634610028c0b8cf9540bcd764bca8
Brande George <george.brande@gmail.com>
2018-09-11 23:07:27 +0300

    mici modif

--- insert.py
+++ insert.py
@@ -18,28 +18,31 @@ with open('ord10.txt', 'r') as f:
         elif row[34] == '2':
             content = list(row[i] for i in included)
             ordine_modif.append(tuple(content))
-    print (ordine_modif)
+##    print (ordine_modif)
         
 
 
 def insert_customer(customer):
 
-    sql = """INSERT INTO ord8(status, order_no, simbol, simbol_type, market, ef_time, side, price, volum, order_term, ticket, update_type, update_time, trader, internal_account, cant_exec, order_status) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-    conn = None
-    try:
-        params = config()
-        conn = psycopg2.connect(**params)
-        cur = conn.cursor()
-        cur.execute(sql, customer)
+   sql = """INSERT INTO ord8(status, order_no, simbol, simbol_type, market, ef_time, side, price, volum, order_term, ticket, update_type, update_time, trader, iacc, cant_exec, order_status) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+   conn = None
+   try:
+       params = config()
+       conn = psycopg2.connect(**params)
+       cur = conn.cursor()
+##       cur.execute(sql, customer)
+       cur.executemany(sql, customer)
 
-        conn.commit()
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-
-if __name__=='__main__':
-    for i in ordine_noi:    
-        insert_customer(i)
+       conn.commit()
+       cur.close()
+   except (Exception, psycopg2.DatabaseError) as error:
+       print(error)
+   finally:
+       if conn is not None:
+           conn.close()
+           
+print (insert_customer(ordine_noi))
+##
+##if __name__=='__main__':
+##   for i in ordine_noi:    
+##       insert_customer(i)
